55110322fb5f74ba359e35aec86bfc3e9a4745f5
IlhamAbaslı <ilhamra@code.edu.az>
2024-01-28 01:19:26 +0400

    practice added

---
+++ assets/js/script.js
@@ -0,0 +1,187 @@
+"use strict";
+
+
+// console.log("Hello World!");
+
+// var surname = "Huseynov";
+// console.log(surname);
+
+// let address = "Ehmedli"
+// console.log(address);
+
+
+// const age = 27;
+// console.log(age);
+
+// const name = "test"
+
+// console.log(name);
+
+
+
+// let surname;
+  
+// console.log(surname);
+
+
+
+// let isMarried = true;
+
+// isMarried = 100;
+
+// console.log(isMarried);
+
+
+
+// let students = [1,2,3,4,5,6,7,8]
+
+// console.log(students);
+
+
+
+// const student = {
+//     name: "Semed",
+//     surname: "Huseynov",
+//     age: 27,
+// }
+
+
+// console.log(student.name + " " + student.surname);
+
+// student.name = "test";
+
+// console.log(student.name + "-" + student.surname);
+
+
+
+
+// let name = "XXX";
+// let surname = "YYY";
+
+//let fullName = name + surname;
+
+// let fullName = `${name} ${surname}`
+// console.log(fullName);
+
+
+
+// let ages = [12,20,30,40,50];
+
+// console.log(ages[ages.length - 1]);
+
+
+// console.log(100+5);
+
+
+// let num1 = 10;
+// let num2 = 3;
+
+// let result = num1 + num2;
+
+// console.log(result);
+
+
+// let num3 = 5**3;
+// let num4 = 5%3;
+
+
+// num3++;
+
+// console.log(num3);
+// console.log(num4);
+
+
+
+// let num1 = "55";
+// let num2 = 55;
+
+// let result = num1 + num2;
+
+// console.log(result);
+
+
+// let num1 = "55";
+
+// let num2 = 55;
+
+// console.log(num1 == num2);
+
+
+
+// let num = 100;
+
+// num *= 2;
+// console.log(num);
+
+
+// let num1 = "55";
+
+// let num2 = 55;
+
+// console.log(num1 === num2);
+
+
+// let number = 100;
+
+// if(number > 90 && number < 120){
+//     console.log("Yes")
+// }else{
+//     console.log("No")
+// }
+
+
+// let result = number == 100 ? "yes" : "no";
+
+// let weekDay = 3;
+
+// switch (weekDay) {
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+        
+//     case 1:
+//         console.log("Monday");
+//         break
+//     default:
+//         console.log("Week day not found");
+//         break;
+// }
+
+
+
+// let isMarried = false;
+
+
+// if(isMarried){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+// debugger
+
+// let nums = [1,2,3,4,30]
+
+// if(nums[nums.length - 1] === 30){
+//     console.log(nums[nums.length - 1]);
+// }else{
+//     console.log("not equal");
+// }
+
+
+
+let nums = [1,2,3,4,5];
+
+// for (let i = 0; i < nums.length; i++) {
+//     console.log(nums[i]);
+    
+// }
+
+
+let sum = 0;
+
+for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+}
+
+console.log(sum);
